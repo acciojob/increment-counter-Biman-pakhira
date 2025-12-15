a7bf6c18f989ff1547c07a20478ba9b0ad49cb63
Biman Pakhira <bimanpakhira2@gmail.com>
2025-12-15 23:19:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const incre = document.querySelector("#counter");
+const incrbtn = document.querySelector("#increment");
+incrbtn.addEventListener("click", ()=>{
+	let currentValue= Number(incre.innerHTML);
+	alert(currentValue);
+	incre.innerText = currentValue+1;
+})
